bb896c761b2e5ee5abcd36925798d7729a3ee6f9
Travis Baumann <thba223@uky.edu>
2016-10-28 14:59:02 -0400

    migration from my grocery list

--- client/app/app.module.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-import appComponent from './app.component';
-
-const AppModule = angular.module('app', [
-])
-    .component('app', appComponent);
-
-export default AppModule;
